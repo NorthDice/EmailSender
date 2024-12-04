a29b0654e53d898508a17e194f108f2a02c61a3c
Balychev Kiril <mister.balychev@gmail.com>
2024-12-04 21:01:05 +0200

    Added DependencyContainer

---
+++ EmailSender/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using EmailSender;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyContainer
+{
+    public static IServiceCollection Configure(IServiceCollection services)
+    {
+
+        services.AddTransient<NotificationService>();
+        services.AddTransient<IMessageService, EmailService>();
+        services.AddTransient<ILogger, ConsoleLogger>();
+
+        return services;
+    }
+}
