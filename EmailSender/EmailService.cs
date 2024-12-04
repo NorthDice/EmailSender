3c16f795f667d1489dedc846dfc8e246aefd8ec2
Balychev Kiril <mister.balychev@gmail.com>
2024-12-04 19:49:39 +0200

    Implemented logging

--- EmailSender/EmailService.cs
+++ EmailSender/EmailService.cs
@@ -8,9 +8,18 @@ namespace EmailSender
 {
     public class EmailService : IMessageService
     {
+        private readonly ILogger _logger;
+
+        public EmailService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public void SendMessage(string message)
         {
             Console.WriteLine("Message sended!");
+
+            _logger.Log("Message loged!");
         }
     }
 }
